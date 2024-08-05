e2acd2749390e14a27824d9ad4818f4ee13b898f
Rahdee <Rahdee99@gmail.com>
2024-08-05 16:58:22 +0200

    Added My Branch

---
+++ ExampleB2C/ExampleB2C/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExampleB2C.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
