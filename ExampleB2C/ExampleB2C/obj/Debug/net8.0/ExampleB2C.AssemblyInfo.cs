1976096b95e5aeac7727fdd58ae8cbd5ba7ff5e5
Froggokouas <161583120+Froggokouas@users.noreply.github.com>
2024-08-06 12:09:06 +0200

    Add DbContext

--- ExampleB2C/ExampleB2C/obj/Debug/net8.0/ExampleB2C.AssemblyInfo.cs
+++ ExampleB2C/ExampleB2C/obj/Debug/net8.0/ExampleB2C.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExampleB2C")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df118d87c0af9fb50f31d7208f35b554ca87fd60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56904a530236bbba3643d5c1f8f36cb455f51685")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExampleB2C")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExampleB2C")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
